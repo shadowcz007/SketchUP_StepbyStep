7860f646d172101379c43f1816930b3123731e9f
shadow <389570357@qq.com>
2018-02-26 16:08:22 +0800

    Update 01-.rb

--- code/01-.rb
+++ code/01-.rb
@@ -12,3 +12,12 @@ point2 = [100,100,100]
 point3 = [0,100,100]
 
 entities.add_face(point1, point2, point3, point1)
+
+
+point1 = [0,0,0]
+point2 = [100,100,100]
+point3 = [0,100,100]
+
+entities.add_cpoint(point1)
+entities.add_cpoint(point2)
+entities.add_cpoint(point3)
