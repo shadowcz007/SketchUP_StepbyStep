bbf4dc938b6a5860dea3a0ce6af23c6af41511e7
shadow <389570357@qq.com>
2018-02-26 16:09:36 +0800

    Update 01-.rb

--- code/01-.rb
+++ code/01-.rb
@@ -1,12 +1,13 @@
 model = Sketchup.active_model
 entities = model.active_entities
 
+#Adding Geometry
 point1 = [0,0,0]
 point2 = [100,100,100]
 
 entities.add_edges(point1, point2)
 
-
+#Adding a Face
 point1 = [0,0,0]
 point2 = [100,100,100]
 point3 = [0,100,100]
@@ -14,6 +15,7 @@ point3 = [0,100,100]
 entities.add_face(point1, point2, point3, point1)
 
 
+#Adding a Construction Point
 point1 = [0,0,0]
 point2 = [100,100,100]
 point3 = [0,100,100]
