645018d81faadc2b31c01815dee453e1c90143cd
shadow <389570357@qq.com>
2018-03-04 17:53:23 +0800

    Update 02.rb

--- code/02.rb
+++ code/02.rb
@@ -1,3 +1,24 @@
+model = Sketchup.active_model
+entities = model.active_entities
+
+sel = model.selection
+
+sel.each do |s|
+ name=s.typename
+  if name=='Face'
+     puts 'Face'
+     puts s.vertices
+  else
+     puts 'Verticrs'
+  end
+end
+
+
+
+
+
+#
+
 x=1
 unless x>2
    puts "x 小于 2"
