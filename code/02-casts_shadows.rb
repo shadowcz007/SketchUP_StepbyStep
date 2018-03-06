17674f3d8049fee9c3b363c19d1573d0014f2dc5
shadow <389570357@qq.com>
2018-03-06 22:06:10 +0800

    Update 02-casts_shadows.rb

--- code/02-casts_shadows.rb
+++ code/02-casts_shadows.rb
@@ -1,27 +1,57 @@
 model = Sketchup.active_model
+entities = model.active_entities
 
-def createGridByHeight(_size,_grid,_height)
+
+def createGridByHeight(_originPoint,_size,_grid,_height)
     _w=_size["width"]
     _h=_size["height"]
     _z=_height
     
-    _gw=_gird["width"]
+    _gw=_grid["width"]
     _gh=_grid["height"]
     
-    _xNum=floor(_w/_gw)
-    _yNum=floor(_h/_gh)
+    _xNum=(_w/_gw).floor.to_f
+    _yNum=(_h/_gh).floor.to_f
     
-    p _xNum
+    _paddingX=(_w-_xNum*_gw)/2
+    _paddingY=(_h-_yNum*_gh)/2
     
+    _points=[]
+    
+    for i in 0..(_xNum-1)
+       # p i  
+       _pointX= _paddingX+_originPoint[0]+_gw*i
+      
+       for j in 0..(_yNum-1)
+         _pointY=_paddingY+_originPoint[0]+_gh*j
+         _point=[_pointX,_pointY,_z]
+         _points.push(_point)
+       end
+       
+    end
+   # p _points
+    return _points
 end
 
+
+_originPoint=[0,0,0]
+
 _size={
-  "width"=>100,
-  "height"=>100
+  "width"=>2000,
+  "height"=>2000
   }
 _grid={
-  "width"=>10,
-  "height"=>10
+  "width"=>110,
+  "height"=>110
   }
-_height=10
-createGridByHeight(_size,_grid,_height)
+_height=3000
+
+#ruby里，哈希（Hash）是类似 "key" => "value" 这样的键值对集合。JS是Object对象，Python是字典，Objective-C也是叫字典。
+
+
+points=createGridByHeight(_originPoint,_size,_grid,_height)
+
+points.each do |p|
+  entities.add_cpoint p
+end
+
