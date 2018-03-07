a21b5e9535c82201d2096b8d3524f0521d765591
shadow <389570357@qq.com>
2018-03-07 09:18:39 +0800

    Update 02-casts_shadows.rb

--- code/02-casts_shadows.rb
+++ code/02-casts_shadows.rb
@@ -2,10 +2,10 @@ model = Sketchup.active_model
 entities = model.active_entities
 
 
-def createGridByHeight(_originPoint,_size,_grid,_height)
+def createGridByHeight(_originPoint,_size,_grid)
     _w=_size["width"]
     _h=_size["height"]
-    _z=_height
+    _z=_originPoint[2]
     
     _gw=_grid["width"]
     _gh=_grid["height"]
@@ -16,16 +16,27 @@ def createGridByHeight(_originPoint,_size,_grid,_height)
     _paddingX=(_w-_xNum*_gw)/2
     _paddingY=(_h-_yNum*_gh)/2
     
-    _points=[]
+     _points=[]
     
-    for i in 0..(_xNum-1)
+    for i in 1..(_xNum-1)
        # p i  
-       _pointX= _paddingX+_originPoint[0]+_gw*i
-      
-       for j in 0..(_yNum-1)
-         _pointY=_paddingY+_originPoint[0]+_gh*j
-         _point=[_pointX,_pointY,_z]
-         _points.push(_point)
+       _pointX1= _paddingX+_originPoint[0]+_gw*(i-1)
+       _pointX2= _paddingX+_originPoint[0]+_gw*i
+       
+       for j in 1..(_yNum-1)
+         _pointY1=_paddingY+_originPoint[0]+_gh*(j-1)
+         _pointY2=_paddingY+_originPoint[0]+_gh*j
+         
+         _point1=[_pointX1,_pointY1,_z]
+         _point2=[_pointX1,_pointY2,_z]
+         _point3=[_pointX2,_pointY2,_z]
+         _point4=[_pointX2,_pointY1,_z]
+         
+          _points.push([_point1,_point2,_point3,_point4])
+          if j==1 && i==1
+           #调试用的
+           # p [_point1,_point2,_point3,_point4]
+          end
        end
        
     end
@@ -37,21 +48,26 @@ end
 _originPoint=[0,0,0]
 
 _size={
-  "width"=>2000,
-  "height"=>2000
+  "width"=>6000,
+  "height"=>3000
   }
 _grid={
-  "width"=>110,
-  "height"=>110
+  "width"=>100,
+  "height"=>100
   }
 _height=3000
+_seed=12
 
 #ruby里，哈希（Hash）是类似 "key" => "value" 这样的键值对集合。JS是Object对象，Python是字典，Objective-C也是叫字典。
 
 
-points=createGridByHeight(_originPoint,_size,_grid,_height)
+points=createGridByHeight(_originPoint,_size,_grid)
+#p points
 
-points.each do |p|
-  entities.add_cpoint p
+points.each do |ps|
+  #entities.add_cpoint p
+  face=entities.add_face(ps)
+  point2 = Geom::Point3d.new(0, 0, _height)
+  edge = entities.add_line(_originPoint, point2)
+  face.followme(edge)
 end
-
